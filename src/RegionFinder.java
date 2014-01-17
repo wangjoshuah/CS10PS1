3324e89850400398ad662524a253b966edfd0e0d
wangjoshuah <joshua.h.wang.15@dartmouth.edu>
2014-01-17 17:06:36 -0500

    working on region finder
    can’t get the region to size up

--- src/RegionFinder.java
+++ src/RegionFinder.java
@@ -11,9 +11,11 @@ public class RegionFinder {
 	private static final int maxColorDiff = 20;				// how similar a pixel color must be to the target color, to belong to a region
 	private static final int minRegion = 50; 				// how many points in a region to be worth considering
 	
-	private ArrayList<ArrayList<Point>> regions;			// a region is a list of points
+	private ArrayList<ArrayList<Point>> regions = new ArrayList<ArrayList<Point>>();			// a region is a list of points
 															// so the identified regions are in a list of lists of points
-
+	private boolean[][] checkedPoints;
+	private ArrayList<Point> toDoList;
+	
 	/**
 	 * Accesses the currently-identified regions.
 	 * @return
@@ -24,13 +26,35 @@ public class RegionFinder {
 	
 	/**
 	 * Tests whether the two colors are "similar enough" (your definition, subject to the static threshold).
-	 * @param c1
-	 * @param c2
+	 * @param c1 the original color
+	 * @param c2 the neighbor color
 	 * @return
 	 */
 	private static boolean colorMatch(Color c1, Color c2) {
 		// YOUR CODE HERE
-		return true; // replace this -- just there so Eclipse won't complain
+		System.out.println("Test Color");
+		if (c2.getRed() - c1.getRed() > maxColorDiff) { //if the neighbor color is 20 greater than the original
+			return false; //go away
+		}
+		else if (c2.getRed() - c1.getRed() < -maxColorDiff) { //if the neighbor color is 20 less than the original
+			return false; //still not true
+		}
+		else if (c2.getBlue() - c1.getBlue() > maxColorDiff) { //repeat for blue
+			return false;
+		}
+		else if (c2.getBlue() - c1.getBlue() < -maxColorDiff) {
+			return false;
+		}
+		else if (c2.getGreen() - c1.getGreen() > maxColorDiff) { //repeat for green
+			return false;
+		}
+		else if (c2.getGreen() - c1.getGreen() < -maxColorDiff) {
+			return false;
+		}
+		else { //it completely works now and isn't outside of any bounds
+			System.out.println("Matches!");
+			return true; // replace this -- just there so Eclipse won't complain
+		}
 	}
 
 	/**
@@ -40,6 +64,53 @@ public class RegionFinder {
 	 */
 	public void findRegions(BufferedImage image, Color color) {
 		// YOUR CODE HERE
+		System.out.println("start finding regions");
+		checkedPoints = new boolean[image.getWidth()][image.getHeight()]; //get a new 2d array of points to check
+		for (int x = 0; x < image.getWidth(); x++) { //for all x's
+			for (int y = 0; y < image.getHeight(); y ++) { //for all y's
+				Point currentPixel = new Point(x,y); //get the pixel at that point
+				Color pixelColor = new Color(image.getRGB(x,y)); //get the color for that
+				if (!checkedPoints[currentPixel.x][currentPixel.y] && colorMatch(color, pixelColor)) { //if the pixel is of the correct color we're looking for,
+					ArrayList<Point> queueToCheck = new ArrayList<Point>(); //create a queue of pixels to check
+					queueToCheck.add(currentPixel); //add the current pixel to that list
+					ArrayList<Point> region = new ArrayList<Point>(); //start a new region to hold it
+					while (!queueToCheck.isEmpty()) { //while we still have points to check
+						System.out.println("while looping");
+						Point neighbor = queueToCheck.get(0); //get the first one int the ArrayList to check add it
+						region.add(neighbor); //add it to the region
+//						checkedPoints[neighbor.x][neighbor.y] = true; //make add it to the checked region
+						addNeighbors(image, neighbor, queueToCheck, color); //add its neighbors to the region we need to check
+						System.out.println("Queue size is " + queueToCheck.size());
+						queueToCheck.remove(0); //remove the point we just checked
+					}
+					System.out.println("Region size is " + region.size());
+					if (region.size() >= minRegion) { //if the region we just checked is greater than the required size
+						regions.add(region); //add it to the list of regions we maintain
+						System.out.println("Add this region to regions");
+					}
+				}
+				checkedPoints[currentPixel.x][currentPixel.y] = true; //make sure we've checked that pixel
+			}
+		}
+	}
+	
+	private void addNeighbors(BufferedImage image, Point pixel, ArrayList<Point> toDoList, Color matchColor) {
+		System.out.println("Adding Neighbors");
+		for (int x = Math.max(0,(int) pixel.getX() - 1); x <= Math.min(image.getWidth(), (int) pixel.getX()); x ++) { //loop through neighboring columns
+			System.out.println("X is " + x + "End X is " + Math.min(image.getWidth(), (int) pixel.getX()));
+			for (int y = Math.max(0, (int) pixel.getY() - 1); y <= Math.min(image.getHeight(), (int) pixel.getY()); y ++) { //loop through neighboring rows
+				System.out.println("Y is " + y + "End Y is " + Math.min(image.getHeight(), (int) pixel.getY()));
+				if (!checkedPoints[x][y]) { //if we haven't checked it yet
+//					checkedPoints[x][y] = true; //tell our algorithm we've checked it so we don't come back
+					Color pixelColor = new Color(image.getRGB(x,y)); //get the color for that
+					System.out.println("Color we are trying to look at is " + pixelColor.toString());
+					if (colorMatch(matchColor, pixelColor)) { //if it's the right color we're looking for,
+						toDoList.add(pixel); //add it to our to do list
+					}	
+				}
+			}
+		}
+		System.out.println("To DO List Size is " + toDoList.size());
 	}
 	
 	/**
@@ -48,5 +119,12 @@ public class RegionFinder {
 	 */
 	public void recolorRegions(BufferedImage image) {
 		// YOUR CODE HERE
+		System.out.println("Recoloring");
+		Color crazyColor = new Color(255, 0, 0);
+		for (ArrayList<Point> region : regions) {
+			for (Point pixel : region) {
+				image.setRGB(pixel.x, pixel.y, crazyColor.getRGB());
+			}
+		}
 	}
 }
--- src/RegionFinderTest.java
+++ src/RegionFinderTest.java
@@ -13,9 +13,11 @@ public class RegionFinderTest extends DrawingFrame {
 	 */
 	public RegionFinderTest(String filename, Color color) {
 		super("Region finder test", filename);
+		System.out.println("put up the picture");
 		
 		// Do the region finding and recolor the image.
 		RegionFinder finder = new RegionFinder();
+		System.out.println("created the region finder");
 		finder.findRegions(image, color);
 		finder.recolorRegions(image);
 	}
@@ -27,7 +29,7 @@ public class RegionFinderTest extends DrawingFrame {
 	public static void main(String[] args) { 
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-//				new RegionFinderTest("pictures/smiley.png", new Color(0, 0, 0));
+				new RegionFinderTest("pictures/smiley.png", new Color(0, 0, 0));
 				new RegionFinderTest("pictures/baker.jpg", new Color(130, 100, 100));
 			}
 		});
